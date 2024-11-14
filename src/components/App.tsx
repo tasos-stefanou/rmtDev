62e67cf97d707e16e4bdef6c7f7286d2666bdae5
Tasos Stefanou <tasos.stefanou.se@gmail.com>
2024-11-14 16:32:45 +0200

    create basic layout of app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,25 @@
+import Background from './Background';
+import Header from './Header';
+import Logo from './Logo';
+import BookmarksButton from './BookmarksButton';
+import SearchForm from './SearchForm';
+import Container from './Container';
+import Sidebar from './Sidebar';
+import ResultsCount from './ResultsCount';
+import SortingControls from './SortingControls';
+import PaginationControls from './PaginationControls';
+import JobItemContent from './JobItemContent';
+import Footer from './Footer';
+
 function App() {
-  return <>Hello!</>;
+  return (
+    <>
+      <Background />
+      <Header />
+      <Container />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
